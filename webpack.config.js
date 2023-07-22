d002f7348f6777ac782c602c8339ef319bdb8a1d
frankxiong <757134184@qq.com>
2023-07-22 18:55:54 +0800

    npm init 创建 package.json, index.tsx可以打包成 index.js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+module.exports = {
+    mode: 'production',
+    entry: {
+        // 入口
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'react-wheel-making',
+        libraryTarget: "umd"
+    },
+    module: {
+        rules: [
+            {test: /\.tsx?$/, loader: "awesome-typescript-loader"}
+        ]
+    }
+}
