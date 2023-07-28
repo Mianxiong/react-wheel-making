073814ee18d220739512ff97856124c5644d811e
frankxiong <757134184@qq.com>
2023-07-28 11:40:51 +0800

    feature: 创建icon目录并进行icon测试

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,16 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import Icon from "./icon"
+import Icon from "./icon/icon";
 
+const fn: React.MouseEventHandler = (e) => {
+    console.log(e.target);
+};
 
 ReactDOM.render(<div>
-    <Icon name="wechat"></Icon>
-    <Icon name="alipay"></Icon>
-    <Icon name="QQ"></Icon>
-</div>, document.querySelector("#root"))
+    <Icon name="qq"
+          onClick={fn}
+          onMouseEnter={() => console.log("enter")}
+          onMouseLeave={() => console.log("leave")}
+          onTouchStart={() => console.log("touch")}
+    />
+</div>, document.querySelector("#root"));
